1f5860d37b09e2675e520d4cf02cd29ac7589a82
Shiven <65511405+Wellick0000@users.noreply.github.com>
2023-02-01 22:41:46 +0530

    Create assihnment_1.cpp

---
+++ assihnment_1.cpp
@@ -0,0 +1,73 @@
+////NOTE:- Kindly run each program seperately after removing "//"
+
+// //(1)Take 2 integer values in two variables x and y and print their product.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x,y;
+//     cout<<"Enter two number"<<endl;
+//     cin>>x>>y;
+//     cout<<"Value of x is "<<x<<" and y is "<<y<<endl;
+// }
+
+// //(2)Print the ASCII value of character ‘U’.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char c = 'U';
+//     int s = c;
+//     cout<<"For "<<c<<" ASCII is "<<s;
+// }
+
+// //(3)Write a C++ program to take length and breadth of a rectangle and print its area.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//        int lenght, breadth;
+//        cout<<"Write the length and breadth"<<endl;
+//        cin>>lenght>>breadth;
+//        cout<<"The area of is "<<lenght*breadth<<endl;
+// }
+
+// //(4)Write a C++ program to calculate the cube of a number.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//        int num;
+//        cout<<"Write the number to find cube"<<endl;
+//        cin>>num;
+//        cout<<"The cube of the number is "<<num*num*num<<endl;
+// }
+
+// //(5)Write a C++ program to find size of basic data types.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//        int a;
+//        char b;
+//        double c;
+//        int short d;
+//        int long e;
+//        cout<<"The size for int is "<<sizeof(a)<<endl;
+//        cout<<"The size for char is "<<sizeof(b)<<endl;
+//        cout<<"The size for double is "<<sizeof(c)<<endl;
+//        cout<<"The size for int short is "<<sizeof(d)<<endl;
+//        cout<<"The size for int long is "<<sizeof(e)<<endl;
+// }
+
+// //(6)Write a C++ program to swap two numbers with the help of a third variable.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a, b, temp;
+//     cout<<"Write the value of a and b before swap "<<endl;
+//     cin>>a>>b;
+//     cout<<"The value of a and b before swap "<<a<<"  and  "<<b<<endl;
+//     temp=a;
+//     a = b;
+//     b = temp;
+//     cout<<"The value of a and b after swap "<<a<<"  and  "<<b<<endl;
+
+        
+
+// }
